c0c8ea53ea6cf1d1eed6d4bf329f3719e4adf9f3
dubidubab1234 <lsbmoon78@gmail.com>
2022-07-13 08:15:34 +0900

    Edit Image file function added

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,19 @@
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
-import { deleteObject, ref } from "firebase/storage";
-import React, { useState } from "react";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadString,
+} from "firebase/storage";
+import React, { useEffect, useState } from "react";
 import { myDB, storageService } from "../myBase";
+import ImageFileInput from "./ImageFileInput";
+import { uuid } from "uuidv4";
 
 function Tweet({ contentInfo, userObj }) {
   const [editing, setEditing] = useState(false);
   const [editInputValue, setEditInputValue] = useState(contentInfo.text);
+  const [editImage, setEditImage] = useState(contentInfo.attachmentUrl);
 
   const handleDelete = async () => {
     if (contentInfo.attachmentReference) {
@@ -31,10 +39,41 @@ function Tweet({ contentInfo, userObj }) {
 
   const handleUpdate = async (e) => {
     e.preventDefault();
-    const wantToEdit = doc(myDB, "tweets", contentInfo.id);
-    await updateDoc(wantToEdit, {
-      text: editInputValue,
-    });
+    let attachmentUrl = null;
+    let attachmentReference = "";
+    if (editImage) {
+      const dataType = editImage.substr(0, 5);
+      if (dataType == "data:") {
+        // 기존 이미지 삭제
+        const imgRef = ref(storageService, contentInfo.attachmentReference);
+        deleteObject(imgRef);
+
+        //새로운 이미지를 입력했을 때
+
+        attachmentReference = `${userObj.uid}/${uuid()}`;
+        const attachmentRef = ref(storageService, attachmentReference);
+        const response = await uploadString(
+          attachmentRef,
+          editImage,
+          "data_url"
+        );
+        attachmentUrl = await getDownloadURL(
+          ref(storageService, attachmentRef)
+        );
+
+        const wantToEdit = doc(myDB, "tweets", contentInfo.id);
+        await updateDoc(wantToEdit, {
+          text: editInputValue,
+          attachmentUrl,
+          attachmentReference,
+        });
+      }
+    } else {
+      const wantToEdit = doc(myDB, "tweets", contentInfo.id);
+      await updateDoc(wantToEdit, {
+        text: editInputValue,
+      });
+    }
     setEditing(false);
   };
 
@@ -59,7 +98,11 @@ function Tweet({ contentInfo, userObj }) {
               placeholder="Edit your sweet"
               onChange={handleEditInput}
             />
-            <button>update</button>
+            <ImageFileInput
+              attachment={editImage}
+              setAttachment={setEditImage}
+            />
+            <input type="submit" value="update" />
           </form>
         </>
       )}
