d9e99245bfe2c5f06b620ca599f8446b57ff531f
dubidubab1234 <lsbmoon78@gmail.com>
2022-07-12 17:59:37 +0900

    Edit Function Added

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,11 @@
-import { deleteDoc, doc } from "firebase/firestore";
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 import { myStorage } from "../myBase";
 
 function Tweet({ contentInfo, userObj }) {
+  const [editing, setEditing] = useState(false);
+  const [editInputValue, setEditInputValue] = useState(contentInfo.text);
+
   const handleDelete = async () => {
     await deleteDoc(doc(myStorage, "tweets", contentInfo.id));
   };
@@ -12,15 +15,46 @@ function Tweet({ contentInfo, userObj }) {
     isOwner = true;
   }
 
-  const handleEdit = () => {};
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleEditInput = (e) => {
+    const newText = e.target.value;
+    setEditInputValue(newText);
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const wantToEdit = doc(myStorage, "tweets", contentInfo.id);
+    await updateDoc(wantToEdit, {
+      text: editInputValue,
+    });
+    setEditing(false);
+  };
 
   return (
     <div>
-      <h4>{contentInfo.text}</h4>
-      {isOwner && (
+      {!editing ? (
         <>
-          <button onClick={handleDelete}>delete</button>
-          <button onClick={handleEdit}>edit</button>
+          <h4>{contentInfo.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={handleDelete}>delete</button>
+              <button onClick={handleEdit}>edit</button>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <form onSubmit={handleUpdate}>
+            <input
+              value={editInputValue}
+              placeholder="Edit your sweet"
+              onChange={handleEditInput}
+            />
+            <button>update</button>
+          </form>
         </>
       )}
     </div>
