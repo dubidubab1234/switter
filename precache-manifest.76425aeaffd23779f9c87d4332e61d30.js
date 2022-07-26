2c3558c061191a906316507343a1333e0419944b
dubidubab1234 <lsbmoon78@gmail.com>
2022-07-27 01:27:05 +0900

    Updates

---
+++ precache-manifest.76425aeaffd23779f9c87d4332e61d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7719fb6aa313097766b",
+    "url": "/switter/static/css/main.36ee76c4.chunk.css"
+  },
+  {
+    "revision": "e7719fb6aa313097766b",
+    "url": "/switter/static/js/main.e7719fb6.chunk.js"
+  },
+  {
+    "revision": "3236c480a6b162aa5833",
+    "url": "/switter/static/js/1.3236c480.chunk.js"
+  },
+  {
+    "revision": "aa38796f20191a28dcea",
+    "url": "/switter/static/js/runtime~main.aa38796f.js"
+  },
+  {
+    "revision": "3c7f387cf04b304c29f8acbb8bbbc08a",
+    "url": "/switter/index.html"
+  }
+];
