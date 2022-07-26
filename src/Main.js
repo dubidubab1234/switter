b14b4f53b6bf53f72a9e22f944f0c33806df8a7a
dubidubab1234 <96683542+dubidubab1234@users.noreply.github.com>
2022-07-27 01:37:04 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,7 @@ import App from "./components/App";
 
 function Main({ isLoggedIn, userObj }) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         {isLoggedIn ? (
           <>
